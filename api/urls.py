a2dfb7f2db1ea09c42cbb267fba81de24a8bf602
Igor Grosu <igrgro@gmail.com>
2023-06-27 08:52:00 +0300

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
 	path('elements/', views.ElementsListView.as_view()),
-    path('elements/<symbol>/', views.ElementRetrieveView.as_view()),
+  path('elements/<atomic_number>/', views.ElementRetrieveView.as_view()),
 	path('klasses/', views.KlassesListView.as_view()),
 	path('', views.WorkerView.as_view())
 ]
